7cea3082269ca6724e5e5403a81506e6a1b950b7
Sebastian Gozin <sgo@thinkerit.be>
2012-02-17 20:39:07 +0100

    Experiment success so replacing test

---
+++ test/functional/grails/plugin/functional/spock/TestSpec.groovy
@@ -0,0 +1,12 @@
+package grails.plugin.functional.spock
+
+import spock.lang.Specification
+
+
+class TestSpec extends Specification {
+
+    def "this spec should be executed"() {
+        expect:
+        true
+    }
+}
